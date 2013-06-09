18ab648af54cdeafdd9aa32273f9839226de5925
Rob Griffiths <rob@bytespider.eu>
2013-06-09 15:35:21 +0200

    Make sure example matches current API

--- example.js
+++ example.js
@@ -1,13 +1,12 @@
 var torque = require('torque');
 
-var board = new torque.Board({port: '/dev/cu.usbmodem411'});
+var board = new torque.Board({port: '/dev/tty.usbmodem411'});
 board.on('ready', function () {
     var servo = new torque.servo({
         range: [ 0, 180 ],
         startAngle: 90,
-        channel: 10 // attach servo to channel 10
+        pin: 10 // attach servo to pin 10
     });
-    board.attach(servo);
 
     servo.center(); // center the servo to center of range
     servo.move(180, 1000); // move servo to angle, and take 1000ms to do so
@@ -16,10 +15,6 @@ board.on('ready', function () {
 
 
     var degrees = 10;
-    // "move" events fire after a successful move.
-    servo.on('move', function (err, degrees) {
-        console.log( 'move', degrees );
-    });
 
     this.loop(25, function () {
         servo.move(degrees, 20);
